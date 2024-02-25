19d3492526e89021822a5ae041a826615da2053d
Pascal Wittler <git@pascalwittler.de>
2024-02-25 19:25:54 +0100

    Add entropialeaks js module

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,7 +1,9 @@
 import TimeModule from './modules/time.js';
 import DateModule from './modules/date.js';
+import EntropiaLeaksModule from './modules/entropialeaks.js';
 import TramDepartureModule from './modules/tram-departure.js';
 
 new TimeModule();
 new DateModule();
+new EntropiaLeaksModule();
 new TramDepartureModule();
---
+++ public/static/js/modules/entropialeaks.js
@@ -0,0 +1,5 @@
+export default class EntropiaLeaksModule {
+  constructor() {
+    this.domElement = document.querySelector('.entropialeaks');
+  }
+}
