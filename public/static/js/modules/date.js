d4088210781abd37ed45d869d016da4c97925638
Pascal Wittler <git@pascalwittler.de>
2024-02-25 11:28:47 +0100

    Add date update js function

--- public/static/js/modules/date.js
+++ public/static/js/modules/date.js
@@ -2,4 +2,17 @@ export default class DateModule {
   constructor() {
     this.domElement = document.querySelector('.date');
   }
+
+  update() {
+    const today = new Date();
+
+    const options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: undefined,
+    };
+
+    this.domElement.innerHTML = `<span>${today.toLocaleDateString('de-DE', options)}</span>`;
+  }
 }
