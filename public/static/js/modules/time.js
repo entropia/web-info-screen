23e8a8f9380d33ee9f784a9f3d8ee3beb85161c6
Pascal Wittler <git@pascalwittler.de>
2024-02-25 11:27:31 +0100

    Add time update js function

--- public/static/js/modules/time.js
+++ public/static/js/modules/time.js
@@ -2,4 +2,16 @@ export default class TimeModule {
   constructor() {
     this.domElement = document.querySelector('.time');
   }
+
+  update() {
+    const now = new Date();
+
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: undefined,
+    };
+
+    this.domElement.innerHTML = `<span>${now.toLocaleTimeString('de-DE', options)}</span>`;
+  }
 }
