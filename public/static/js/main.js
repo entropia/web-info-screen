f7962a1b8f1e979f1248a3e6636014c012dfeaa6
Pascal Wittler <git@pascalwittler.de>
2024-02-25 19:20:18 +0100

    Add tram departure js module

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,5 +1,7 @@
 import TimeModule from './modules/time.js';
 import DateModule from './modules/date.js';
+import TramDepartureModule from './modules/tram-departure.js';
 
 new TimeModule();
 new DateModule();
+new TramDepartureModule();
---
+++ public/static/js/modules/tram-departure.js
@@ -0,0 +1,5 @@
+export default class TramDepartureModule {
+  constructor() {
+    this.domElement = document.querySelector('.tram-departure');
+  }
+}
