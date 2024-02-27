c73376e362b656ccf8bab9cf316f6f78de8f305e
Pascal Wittler <git@pascalwittler.de>
2024-02-27 09:01:32 +0100

    Aggregate api request GET parameters

--- public/api.php
+++ public/api.php
@@ -4,11 +4,18 @@ class Api
 {
     public function __construct()
     {
+        $request_parameters = self::getRequestParameters();
+
         $response = [];
 
         header('Content-Type: application/json;charset=utf-8');
 
         echo json_encode($response, JSON_FORCE_OBJECT);
+    }
+
+    protected static function getRequestParameters(): array
+    {
+        return $_GET;
     }
 }
 
