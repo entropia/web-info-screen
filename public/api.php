4bf711b29546c12d0806c319bdcec895c2d11e16
Pascal Wittler <git@pascalwittler.de>
2024-02-27 09:06:11 +0100

    Add switch function to call API modules

--- public/api.php
+++ public/api.php
@@ -11,11 +11,34 @@ class Api
     {
         $request_parameters = self::getRequestParameters();
 
-        $response = [];
+        $response = self::callModule($request_parameters);
 
         header('Content-Type: application/json;charset=utf-8');
 
         echo json_encode($response, JSON_FORCE_OBJECT);
+    }
+
+    protected static function callModule(array $request_parameters)
+    {
+        switch ($request_parameters['module']) {
+            case 'eantropialeaks':
+                $response = (new EntropiaLeaksModule)->requestData($request_parameters);
+                break;
+
+            case 'events':
+                $response = (new EventsModule)->requestData($request_parameters);
+                break;
+
+            case 'tram-departure':
+                $response = (new TramDepartureModule)->requestData($request_parameters);
+                break;
+
+            case 'weather':
+                $response = (new WeatherModule)->requestData($request_parameters);
+                break;
+        }
+
+        return $response;
     }
 
     protected static function getRequestParameters(): array
