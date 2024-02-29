c2230066309b4c72b23626412e02341f89cdeb93
Pascal Wittler <git@pascalwittler.de>
2024-02-29 20:52:57 +0100

    Add list of relevant tram stops

--- public/static/js/modules/tram-departure.js
+++ public/static/js/modules/tram-departure.js
@@ -1,5 +1,19 @@
 export default class TramDepartureModule {
   constructor() {
     this.domElement = document.querySelector('.tram-departure');
+    this.stops = [
+      {
+        'name': 'Kronenplatz',
+        'id':   '7001002'
+      },
+      {
+        'name': 'Marktplatz',
+        'id':   '7001003'
+      },
+      {
+        'name': 'Rüppurrer Tor',
+        'id':   '7000085'
+      }
+    ];
   }
 }
