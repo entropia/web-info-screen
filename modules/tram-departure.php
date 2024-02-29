b81149e401aedce96cd022d925cef4bfd025522e
Pascal Wittler <git@pascalwittler.de>
2024-02-29 20:47:10 +0100

    Build tram departure API request uri including parameters

--- modules/tram-departure.php
+++ modules/tram-departure.php
@@ -6,6 +6,8 @@ class TramDepartureModule
     const API_GET_PARAMETERS = '?outputFormat=JSON&depType=stopEvents&mode=direct&type_dm=stop&useRealtime=1';
     const API_STOP_ID_GET_PARAMETER = '&name_dm=';
 
+    protected string $request_uri;
+
     public function __construct()
     {
 
@@ -13,6 +15,9 @@ class TramDepartureModule
 
     public function requestData(array $request_parameters)
     {
+        $stop_id = $request_parameters['stop-id'];
+
+        $this->request_uri = self::API_URL . self::API_GET_PARAMETERS . self::API_STOP_ID_GET_PARAMETER . $stop_id;
 
     }
 }
