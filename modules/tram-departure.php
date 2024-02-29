df6d7ce09c8d5784491595636e303fffebb3045b
Pascal Wittler <git@pascalwittler.de>
2024-02-29 20:39:22 +0100

    Add tram departure API request uri parts

--- modules/tram-departure.php
+++ modules/tram-departure.php
@@ -2,6 +2,10 @@
 
 class TramDepartureModule
 {
+    const API_URL = 'https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST';
+    const API_GET_PARAMETERS = '?outputFormat=JSON&depType=stopEvents&mode=direct&type_dm=stop&useRealtime=1';
+    const API_STOP_ID_GET_PARAMETER = '&name_dm=';
+
     public function __construct()
     {
 
