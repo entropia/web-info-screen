8c80bd321b16886503636f465b3d56a1f3e6799f
Pascal Wittler <git@pascalwittler.de>
2024-03-02 06:16:51 +0100

    Add fetchData() function to weather js module

--- public/static/js/modules/weather.js
+++ public/static/js/modules/weather.js
@@ -2,4 +2,17 @@ export default class WeatherModule {
   constructor() {
     this.domElement = document.querySelector('.weather');
   }
+
+  async fetchData() {
+    const parameters = {
+      'module':    'weather',
+      'latitude':  '49.0',
+      'longitude': '8.4',
+      'timezone':  'Europe/Berlin'
+    };
+
+    const response = await fetch(`api.php?${new URLSearchParams(parameters)}`);
+
+    return response.json();
+  }
 }
