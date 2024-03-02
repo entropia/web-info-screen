e633af170a217fcc93ebb1bdaa35f2152c40987c
Pascal Wittler <git@pascalwittler.de>
2024-03-02 04:42:05 +0100

    Add weather api request uri parts as php class constants

--- modules/weather.php
+++ modules/weather.php
@@ -2,6 +2,12 @@
 
 class WeatherModule
 {
+    const API_URL = 'https://api.open-meteo.com/v1/dwd-icon';
+    const API_GET_PARAMETERS_FEATURES = '?hourly=temperature_2m,precipitation,weather_code&forecast_hours=6';
+    const API_GET_PARAMETER_LATITUDE = '&latitude=';
+    const API_GET_PARAMETER_LONGITUDE = '&longitude=';
+    const API_GET_PARAMETER_TIMEZONE = '&timezone=';
+
     public function requestData(array $request_parameters)
     {
 
