622712d769f963b9f9fd63c7db3c2a9b7833fb7e
Pascal Wittler <git@pascalwittler.de>
2024-03-03 13:48:51 +0100

    Build entropialeaks request uri

--- modules/entropialeaks.php
+++ modules/entropialeaks.php
@@ -4,8 +4,13 @@ class EntropiaLeaksModule
 {
     const API_URL = 'https://chaos.social/api/v1/timelines/tag/';
 
+    protected string $request_uri;
+
     public function requestData(array $request_parameters)
     {
+        $hashtag = $request_parameters['hashtag'];
+
+        $this->request_uri = self::API_URL . $hashtag;
 
     }
 }
