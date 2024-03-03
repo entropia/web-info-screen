8ebcef74873a8c6be5bb7999a252490e0adc6fc9
Pascal Wittler <git@pascalwittler.de>
2024-03-03 15:27:58 +0100

    Add entropialeaks fetchData() js function

--- public/static/js/modules/entropialeaks.js
+++ public/static/js/modules/entropialeaks.js
@@ -2,4 +2,15 @@ export default class EntropiaLeaksModule {
   constructor() {
     this.domElement = document.querySelector('.entropialeaks');
   }
+
+  async fetchData() {
+    const parameters = {
+      'module': 'entropialeaks',
+      'hashtag': 'entropialeaks'
+    };
+
+    const response = await fetch(`api.php?${new URLSearchParams(parameters)}`);
+
+    return response.json();
+  }
 }
