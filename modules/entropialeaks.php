6772e916e97d0661ed78733bedeb94657f2157b1
Pascal Wittler <git@pascalwittler.de>
2024-03-03 13:50:14 +0100

    Return entropialeaks API response as JSON

--- modules/entropialeaks.php
+++ modules/entropialeaks.php
@@ -6,11 +6,12 @@ class EntropiaLeaksModule
 
     protected string $request_uri;
 
-    public function requestData(array $request_parameters)
+    public function requestData(array $request_parameters): array
     {
         $hashtag = $request_parameters['hashtag'];
 
         $this->request_uri = self::API_URL . $hashtag;
 
+        return json_decode(file_get_contents($this->request_uri), JSON_OBJECT_AS_ARRAY);
     }
 }
